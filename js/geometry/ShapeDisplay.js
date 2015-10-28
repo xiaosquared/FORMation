c2a1a13608c0290d1fc08bfd714deb90b13a72c7
ZackUhlenhuth <uhlenhuthz@gmail.com>
2015-10-28 16:28:58 -0400

    removed unnecesarry code

--- js/geometry/ShapeDisplay.js
+++ js/geometry/ShapeDisplay.js
@@ -1,6 +1,20 @@
+var materials = (function() {
+    var m = {};
+    var pinBlack = THREE.ImageUtils.loadTexture('textures/darkPinTexture.jpg');
+    m.wallMaterial = new THREE.MeshPhongMaterial( { color: 0xeeeeee, shading: THREE.SmoothShading } );
+    m.clearMaterial = new THREE.MeshPhongMaterial( { color: 0xeeeeee, shading: THREE.SmoothShading, transparent: true, opacity: 0.3 } );
+    m.darkMaterial = new THREE.MeshLambertMaterial( { color: 0x333333, map: pinBlack } );
+    m.touchMaterial = new THREE.MeshLambertMaterial( {color: 0xdd1111 });
+
+    m.shadowMaterial = new THREE.MeshPhongMaterial( {color: 0x666666, shading: THREE.SmoothShading });
+    m.shadowClearMaterial = new THREE.MeshPhongMaterial( {color: 0x666666, shading: THREE.SmoothShading, transparent:true, opacity: 0.3 });
+
+    m.ghostMaterial = new THREE.MeshBasicMaterial({color: 0x999999, transparent: true, opacity: 0.5});
+    return m;
+})();
+
 function ShapeDisplay(xWidth, yWidth, height, scene) {
     this.pinSize = 1.0;
-    //this.inBetween = .0475;
     this.inBetween = 0.;
     this.xWidth = xWidth;
     this.yWidth = yWidth;
@@ -8,19 +22,13 @@ function ShapeDisplay(xWidth, yWidth, height, scene) {
     this.height = height;
     this.container = new THREE.Mesh();
     this.pins = new Array(xWidth * yWidth);
-    this.pinHeight = 4;
+    this.pinHeight = 6;
     this.shadowPins = [];
     this.touchPins = [];
     this.lastPositions = new Array(xWidth & yWidth);
 
-    var pinBlack = THREE.ImageUtils.loadTexture('textures/darkPinTexture.jpg');
-    this.material = new THREE.MeshPhongMaterial( { color: 0xeeeeee, shading: THREE.SmoothShading } );
-    this.clearMaterial = new THREE.MeshPhongMaterial( { color: 0xeeeeee, shading: THREE.SmoothShading, transparent: true, opacity: 0.3 } );
-    this.darkMaterial = new THREE.MeshLambertMaterial( { color: 0x333333, shading: THREE.SmoothShading, map: pinBlack } );
-    this.touchMaterial = new THREE.MeshLambertMaterial( {color: 0xdd1111, shading: THREE.SmoothShading });
-
     for (var i = 0; i < xWidth * yWidth; i++) {
-        var pin = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), this.material);
+        var pin = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), materials.wallMaterial);
         pin.position.set(i% xWidth * (this.pinSize + this.inBetween) + this.pinSize/2,
                         0, Math.floor(i/xWidth) * (this.pinSize + this.inBetween) + this.pinSize/2);
         pin.scale.set(1, this.pinHeight, 1);
@@ -30,8 +38,8 @@ function ShapeDisplay(xWidth, yWidth, height, scene) {
         this.lastPositions[i] = 0;
     }
 
-    var bottom = new THREE.Mesh(new THREE.BoxGeometry(this.getTotalWidth(), 0.1, this.getTotalWidth()),this.darkMaterial);
-    bottom.position.set(this.getTotalWidth()/2, 0, this.getTotalWidth()/2);
+    var bottom = new THREE.Mesh(new THREE.BoxGeometry(this.getTotalWidth(), 0.75, this.getTotalWidth()), materials.darkMaterial);
+    bottom.position.set(this.getTotalWidth()/2, -1, this.getTotalWidth()/2);
     this.container.add(bottom);
 
     if (scene)
@@ -40,6 +48,15 @@ function ShapeDisplay(xWidth, yWidth, height, scene) {
     this.container.rotateY(-Math.PI/2);
     this.container.position.set(this.getTotalWidth(), this.container.position.y, -this.getTotalWidth()/2);
 }
+ShapeDisplay.prototype.getGeometry = function() {
+    return this.container;
+}
+ShapeDisplay.prototype.getWidthInPins = function() {
+    return this.xWidth;
+}
+ShapeDisplay.prototype.getHeightInPins = function() {
+    return this.yWidth;
+}
 ShapeDisplay.prototype.getTotalWidth = function() {
     return this.totalWidth;
 }
@@ -47,7 +64,10 @@ ShapeDisplay.prototype.addToScene = function(scene) {
     scene.add(this.container);
 }
 ShapeDisplay.prototype.getIndex = function(x, y) {
-  return  y * this.xWidth + x % this.xWidth;
+  return  y * this.xWidth + x;
+}
+ShapeDisplay.prototype.getPosition = function() {
+    return this.container.position;
 }
 ShapeDisplay.prototype.setPositionZ = function(z) {
     this.container.position.set(    this.container.position.x,
@@ -59,11 +79,6 @@ ShapeDisplay.prototype.setPositionX = function(x) {
                                     this.container.position.y,
                                     this.container.position.z);
 }
-// ShapeDisplay.prototype.setPositionZ = function(z) {
-//     this.container.position.set(    this.container.position.x,
-//                                     this.container.position.y,
-//                                     z - this.getTotalWidth()/2);
-// }
 ShapeDisplay.prototype.getPinHeight = function(x, y){
     var index = this.getIndex(x, y);
     if (index < this.pins.length)
@@ -73,8 +88,18 @@ ShapeDisplay.prototype.getPinHeight = function(x, y){
 ShapeDisplay.prototype.getPinHeightForPhysical = function(x, y) {
     var index = this.getIndex(x, y);
     if (index < this.pins.length)
-        return this.pins[index].position.y * 255;
+        return this.pins[index].position.y * 255 / this.pinHeight;
     return null;
+}
+ShapeDisplay.prototype.getHeightsMsgForPhysical = function() {
+    var msg = "";
+    for (var x = 0; x < this.xWidth; x++) {
+        for (var y = 0; y < this.yWidth; y++) {
+            var h = this.getPinHeightForPhysical(x, y);
+            msg += x + "," + y + "," + h + "-";
+        }
+    }
+    return msg.substring(0, msg.length -1);
 }
 ShapeDisplay.prototype.setPinHeight = function(x, y, h) {
     if (h || (h == 0)) {
@@ -94,38 +119,21 @@ ShapeDisplay.prototype.setPinHeightFromPhysical = function(x, y, h) {
         this.setPinHeight(x, y/255)
     }
 }
-ShapeDisplay.prototype.getHeightsMsgForPhysical = function() {
-    var msg = "";
-    for (var x = 0; x < this.xWidth; x++) {
-        for (var y = 0; y < this.yWidth; y++) {
-            var h = this.getPinHeightForPhysical(x, y);
-            msg += x + "," + y + "," + h + "-";
-        }
-    }
-    return msg.substring(0, msg.length -1);
-}
 ShapeDisplay.prototype.getActualWidth = function() {
     return (this.pinSize + this.inBetween) * (this.xWidth - 1) + this.pinSize;
 }
 ShapeDisplay.prototype.getActualHeight = function() {
     return (this.pinSize + this.inBetween) * (this.yWidth - 1) + this.pinSize;
 }
-ShapeDisplay.prototype.setPinMaterial = function(x, y, type) {
-    if (type || (type == 0)) {
+ShapeDisplay.prototype.setPinMaterial = function(x, y, material) {
+    if (material)
         var index = this.getIndex(x, y);
-    } else {
+    else {
         index = x;
-        type = y;
-    } if (index < this.pins.length) {
-        if (type == 0)
-            this.pins[index].material = this.material;
-        else if (type == 1)
-            this.pins[index].material = this.darkMaterial;
-        else if (type == 2)
-            this.pins[index].material = this.touchMaterial;
-        else if (type == 3)
-            this.pins[index].material = this.clearMaterial;
+        material = y;
     }
+    if (index < this.pins.length)
+        this.pins[index].material = material;
 }
 
 ShapeDisplay.prototype.setLastPinHeight = function(x, y, height) {
@@ -140,6 +148,17 @@ ShapeDisplay.prototype.getLastPinHeight = function(x, y, height) {
       return this.lastPositions[index];
   return null;
 }
+
+//new stuff
+
+ShapeDisplay.prototype.clearDisplay = function() {
+    for (var i = 0; i < this.xWidth; i++) {
+      for (var j = 0; j < this.yWidth; j++) {
+        this.setPinHeight(i, j, 0);
+      }
+    }
+}
+
 ShapeDisplay.prototype.makeBox = function(x, y, xWidth, yWidth, height) {
   return new Box(x, y, xWidth, yWidth, height, this);
 }
