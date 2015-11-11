a02dd6fea09f3a66b8c2616f55368e8035085722
media_lab <tangible-admin@@media.mit.edu>
2015-11-11 18:31:58 -0500

    touch detection doesn't suck anymorels git push

---
+++ public/js/xform/TouchManager.js
@@ -0,0 +1,11 @@
+var touchManager = {
+    touchQueue : [],
+
+    logTouch : function(data) {
+        console.log("touch! " + JSON.stringify(data));
+    },
+
+    handleNewTouch: function(data) {
+
+    }
+};
